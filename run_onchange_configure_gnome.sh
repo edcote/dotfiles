fe981d71285d18d1debc4d5888999a708e5221e9
Edmond Cote <ecote@google.com>
2026-05-24 09:59:45 -0700

    [desktop] Optimize latency and extract launch wrappers
    - gnome:
      - Add keyboard repeat rate optimizations (delay 250ms, interval 20ms) under a new "Keyboard / Input Settings" section.
      - Update custom keybindings for Vim, Chrome, and Cider-V to use new wrappers in ~/.local/bin.
      - Remove desktop wallpaper management to allow manual control.
      - Align headers.
    - alacritty:
      - Set opacity to 1.0 (opaque) to reduce compositor blending latency.
    - wrappers:
      - Add ~/.local/bin/chrome_wayland wrapper with Wayland and GPU latency flags.
      - Add ~/.local/bin/gvim_wayland wrapper to force Wayland mode.
    
    TAG=agy
    
    CONV=23bf352a-1a88-4b1c-8e43-a2963ed7280b

--- run_onchange_configure_gnome.sh
+++ run_onchange_configure_gnome.sh
@@ -20,27 +20,13 @@ gsettings set org.gnome.desktop.wm.keybindings cycle-windows-backward "['<Shift>
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 
 # ---------------------------------------------------------------------
-# Desktop Settings
+# Keyboard / Input Settings
 # ---------------------------------------------------------------------
-
-# Set Background (use wallpaper.png if exists, else solid black)
-wallpaper_file="${HOME}/wallpaper.png"
-if [ -f "${wallpaper_file}" ]; then
-  echo "Wallpaper file found: ${wallpaper_file}. Setting as background..."
-  gsettings set org.gnome.desktop.background picture-uri "file://${wallpaper_file}"
-  gsettings set org.gnome.desktop.background picture-uri-dark "file://${wallpaper_file}"
-  gsettings set org.gnome.desktop.background picture-options 'scaled'
-  gsettings set org.gnome.desktop.background primary-color '#000000'
-else
-  echo "No wallpaper file found at ${wallpaper_file}. Setting solid black background..."
-  gsettings set org.gnome.desktop.background picture-uri ''
-  gsettings set org.gnome.desktop.background picture-uri-dark ''
-  gsettings set org.gnome.desktop.background primary-color '#000000'
-  gsettings set org.gnome.desktop.background color-shading-type 'solid'
-fi
+gsettings set org.gnome.desktop.peripherals.keyboard delay 250
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 20
 
 # ---------------------------------------------------------------------
-#  Configure Custom Keybindings
+# Configure Custom Keybindings
 # ---------------------------------------------------------------------
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[ \
   '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', \
@@ -58,12 +44,12 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 
 # custom1: Vim
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'vim'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command '/usr/local/google/home/ecote/bin/gvim_wayland'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "$HOME/.local/bin/gvim_wayland"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>g'
 
 # custom2: Chrome
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name 'Chrome'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command '/opt/google/chrome/google-chrome --ozone-platform-hint=wayland --new-window'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command "$HOME/.local/bin/chrome_wayland --new-window"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding '<Super>i'
 
 # custom3: Python
@@ -73,7 +59,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 
 # custom4: Cider-V
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/ name 'Cider-V'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/ command '/opt/google/chrome/google-chrome --ozone-platform-hint=wayland --app=https://cider-v.corp.google.com/?ws=__prompt'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/ command "$HOME/.local/bin/chrome_wayland --app=https://cider-v.corp.google.com/?ws=__prompt"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/ binding '<Super>v'
 
 # custom5: Run in Terminal
